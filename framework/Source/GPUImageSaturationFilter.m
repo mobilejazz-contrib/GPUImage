7a3bdace051f7fb403c0ba0d05f5dd10d5fc935c
Diego Freniche Brito <diego@freniche.com>
2017-07-12 10:09:08 +0200

    Changes introduced for MyMoments project (not contributed to original project)

--- framework/Source/GPUImageSaturationFilter.m
+++ framework/Source/GPUImageSaturationFilter.m
@@ -6,16 +6,16 @@ NSString *const kGPUImageSaturationFragmentShaderString = SHADER_STRING
  varying highp vec2 textureCoordinate;
  
  uniform sampler2D inputImageTexture;
- uniform lowp float saturation;
+ uniform mediump float saturation;
  
  // Values from "Graphics Shaders: Theory and Practice" by Bailey and Cunningham
  const mediump vec3 luminanceWeighting = vec3(0.2125, 0.7154, 0.0721);
  
  void main()
  {
-    lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
-    lowp float luminance = dot(textureColor.rgb, luminanceWeighting);
-    lowp vec3 greyScaleColor = vec3(luminance);
+    mediump vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+    mediump float luminance = dot(textureColor.rgb, luminanceWeighting);
+    mediump vec3 greyScaleColor = vec3(luminance);
     
 	gl_FragColor = vec4(mix(greyScaleColor, textureColor.rgb, saturation), textureColor.w);
 	 
